b8e7d0d1f78b475ceb1a2fd4fc86d13b74aa8e75
sidharth131102 <sidharthunnikrishnan2002@gmail.com>
2025-12-15 19:03:49 +0530

    url fix 3

--- utils/storage_ops.py
+++ utils/storage_ops.py
@@ -1,6 +1,5 @@
 from google.cloud import storage
 import datetime
-from datetime import timedelta
 import os
 
 storage_client = storage.Client()
@@ -10,21 +9,30 @@ VALID_ROLES = ["U", "P", "T"]
 VALID_REMARKS = ["good", "replace"]
 
 def generate_signed_url(request_id, role, remark, wo_id):
-    bucket_name = os.environ["INSPECTION_BUCKET"]
+    """
+    GCS Path:
+    <requestId>/<ROLE>-<REMARK>/<WO_ID>.jpg
+    """
 
-    filename = f"{request_id}/{wo_id}/{role}_{remark}.jpg"
+    role = role.upper()
+    remark = remark.lower()
 
-    client = storage.Client()
-    bucket = client.bucket(bucket_name)
-    blob = bucket.blob(filename)
+    if role not in VALID_ROLES:
+        raise ValueError("Invalid role")
+
+    if remark not in VALID_REMARKS:
+        raise ValueError("Invalid remark")
+
+    folder = f"{role}-{remark}".upper()
+    file_path = f"{request_id}/{folder}/{wo_id}.jpg"
+
+    bucket = storage_client.bucket(BUCKET)
+    blob = bucket.blob(file_path)
 
     url = blob.generate_signed_url(
         version="v4",
-        expiration=timedelta(minutes=15),
-        method="PUT",
-        content_type="application/octet-stream",
-        service_account_email=client._credentials.service_account_email,
-        access_token=client._credentials.token,
+        expiration=datetime.timedelta(minutes=15),
+        method="PUT"
     )
 
     return url
